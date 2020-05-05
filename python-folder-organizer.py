a9770c55d48fc90a7ca0844843bb0a4557790e24
Nikolaj Licht <licht.nikolaj@gmail.com>
2020-05-05 22:30:33 +0200

    managed to detect .lnk files and remove them

--- python-folder-organizer.py
+++ python-folder-organizer.py
@@ -4,14 +4,17 @@ import shutil
 from win10toast import ToastNotifier
 from datetime import datetime, timedelta
 import send2trash
+import PyPDF2
+import textract
 
 pathsToClean = []
 folders = []
 
 def setup ():
-    # path, should I delete after time, should I delete empty folders, should i delete links
-    newPathToClean(Path(r'C:\\', 'Users', 'Nikolaj Licht', 'Downloads'), True, True, True)
-    newPathToClean(Path(r'C:\\', 'Users', 'Nikolaj Licht', 'Desktop'), False, True, True)
+    # path, should I delete after time, should I delete empty folders, should i delete links, should cleanUpFolder
+    newPathToClean(Path(r'C:\\', 'Users', 'Nikolaj Licht', 'Downloads'), True, True, True, True)
+    newPathToClean(Path(r'C:\\', 'Users', 'Nikolaj Licht', 'Desktop'), False, True, True, True)
+    newPathToClean(Path(r'C:\\', 'Users', 'Public', 'Desktop'), False, False, True, False)
 
     # Foldername, lifetime of files, ...filetypes
     registerFolder("Images", 7, 'jpg', 'jpeg', 'gif', 'png', 'ico')
@@ -21,33 +24,37 @@ def setup ():
     registerFolder("Audio", 7, 'wav', 'mp3')
     registerFolder("Video", 7, 'mpeg', 'mov', 'mp4', 'webm', 'avi', 'avchd', 'm4p')
     registerFolder("Builds", 7, 'apk')
-    registerFolder("3D", 7, 'blend', 'shader', 'fbx')
-    registerFolder("Design", 7, 'ai', 'svg', 'afdesign', 'afphoto', 'afphublisher')
+    registerFolder("3D", 7, 'blend', 'fbx', 'obj', 'stl', 'ply', 'dae', '3ds', 'stp', 'ma', 'c4d', 'mb')
+    registerFolder("Game Dev", 7, 'shader', 'asset', 'unity', 'cs', 'tscn')
+    registerFolder("Web", 7, 'php', 'js', 'scss', 'css', 'html')
+    registerFolder("Design", 7, 'ai', 'svg', 'afdesign', 'afphoto', 'afpublisher')
     registerFolder("Fonts", 7, 'otf', 'ttf')
+    registerFolder("Programming", 7, 'py', 'rb')
 
     
 def main ():
     setup()
     
     for path in pathsToClean:
-        if (path["path"].exists()):
-            # Go to paths
-            os.chdir(path["path"])
+        if path["cleanUpPath"]:
+            if (path["path"].exists()):
+                # Go to paths
+                os.chdir(path["path"])
 
 
-            for folder in folders:
-                #create folder if not exists
-                newFilePath = makeNewFolder(folder['name'])
+                for folder in folders:
+                    #create folder if not exists
+                    newFilePath = makeNewFolder(folder['name'])
 
-                for fileType in folder['fileTypes']:
-                    
-                    for messyFile in list(path["path"].glob('*.' + fileType)):
-                        messyPath = Path(messyFile)
-                        shutil.move(messyPath, newFilePath / messyPath.name)
-                        print(messyFile)
+                    for fileType in folder['fileTypes']:
+                        
+                        for messyFile in list(path["path"].glob('*.' + fileType)):
+                            messyPath = Path(messyFile)
+                            shutil.move(messyPath, newFilePath / messyPath.name)
+                            print(messyFile)
 
-        else:
-            print('could not find path: ' + str(path["path"]))
+            else:
+                print('could not find path: ' + str(path["path"]))
 
         if path["deleteFilesAfterTime"]:
             deleteOldFiles(path)
@@ -64,7 +71,7 @@ def makeNewFolder(foldername):
     registerFolderPath = Path.cwd() / foldername
     if not registerFolderPath.is_dir():
         os.makedirs(foldername)
-        print('successfully created new folder' + foldername)
+        print('Created new folder - ' + foldername)
     else:
         print('folder ' + foldername + ' already exists, no new folder added')
     
@@ -92,24 +99,34 @@ def deleteEmptyFolders(path):
                     os.rmdir(path["path"] / folder["name"])
 
 def deleteShortcuts(path):
-    pass
-    
-'''
+    if path["path"].exists():
+        os.chdir(Path(path["path"]))
+        for windowsshortcut in list(Path.cwd().glob('*.lnk')):
+            print(windowsshortcut)
+            send2trash.send2trash(windowsshortcut.name)
+        print(os.listdir())
+
+def sortTextsBasedOnContent(path):
+    '''
+    go to text folder
+    for each text, check against keyword collection
+    put into folder if match
+    consider where folder should be
+
     if path["path"].exists():
         for folder in folders:
             if Path(path["path"] / folder["name"]).exists():
                 os.chdir(Path(path["path"] / folder["name"]))
-                allfiles = os.listdir()
-                #for some reason python cannot find the files that ends with windows .lnk
-'''
+                 
+                for afile in list(Path.cwd().glob('*')):
+                    if os.path.getmtime(str(afile)) < expirationDate:
+                        pass
 
-def sortTextsBasedOnContent(path):
-    '''
-        Make this arguemtn optional like sortTextsOnContent=True
-        For each file that is pdf / text use 
-        https://medium.com/better-programming/how-to-convert-pdfs-into-searchable-key-words-with-python-85aab86c544f
-        for reading the content, and if the content contains keywords (stored in a seperate file) like name, address, order, invoice
-        put into folder with important information
+    Make this arguemtn optional like sortTextsOnContent=True
+    For each file that is pdf / text use 
+    https://medium.com/better-programming/how-to-convert-pdfs-into-searchable-key-words-with-python-85aab86c544f
+    for reading the content, and if the content contains keywords (stored in a seperate file) like name, address, order, invoice
+    put into folder with important information
     '''
 
 def registerFolder(name, fileLifeTimeInDays, *args):
@@ -121,12 +138,13 @@ def registerFolder(name, fileLifeTimeInDays, *args):
         folder['fileTypes'].append(ft)
     folders.append(folder)
 
-def newPathToClean(path, deleteFilesAfterTime, deleteEmptyFolders, deleteShortcuts):
+def newPathToClean(path, deleteFilesAfterTime, deleteEmptyFolders, deleteShortcuts, cleanUpPath):
     newPath = {
             'path': path,
             'deleteFilesAfterTime': deleteFilesAfterTime,
             'deleteEmptyFolders' : deleteEmptyFolders,
-            'deleteShortcuts' : deleteShortcuts
+            'deleteShortcuts' : deleteShortcuts,
+            'cleanUpPath' : cleanUpPath
             }
     pathsToClean.append(newPath)
 
